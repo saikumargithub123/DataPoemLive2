3ff1e98eda8c2bf7f687f8dd8e7aefd077637116
saikumargithub123 <128738938+saikumargithub123@users.noreply.github.com>
2023-04-21 01:09:38 +0530

    Add files via upload

---
+++ DataPoem.js
@@ -0,0 +1,146 @@
+function custom1(){
+
+    var a2=document.getElementById("red")
+    a2.innerHTML="<form><fieldset class="+"b1"+"><fieldset class="+"a2"+"></fieldset><fieldset class="+"g1"+"> <strong class ="+"d1"+"></strong><br><br><br><form>@amyrobson<textarea class="+"i1"+" ></textarea></fieldset><fieldset class="+"a1"+"><center> &nbsp&nbsp &nbsp &nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp<input type="+"submit"+" value="+"reply"+" class="+"k1"+";></center></fieldset></fieldset></form>";
+   
+    
+
+    
+}
+function custom2(){
+
+    var a3=document.getElementById("red1")
+    a3.innerHTML="<form><fieldset class="+"b1"+"><fieldset class="+"a2"+"></fieldset><fieldset class="+"g1"+"> <strong class ="+"d1"+"></strong><br><br><br><form>@maxblagun<textarea class="+"i1"+" ></textarea></fieldset><fieldset class="+"a1"+"><center> &nbsp&nbsp &nbsp &nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp<input type="+"submit"+" value="+"reply"+" class="+"k1"+";></center></fieldset></fieldset></form>";
+   
+    
+
+    
+}
+function custom3(){
+
+    var a3=document.getElementById("red3")
+    a3.innerHTML="<form><fieldset class="+"b1"+"><fieldset class="+"a2"+"></fieldset><fieldset class="+"g1"+"> <strong class ="+"d1"+"></strong><br><br><br><form>@ramsesmiron<textarea class="+"i1"+" ></textarea></fieldset><fieldset class="+"a1"+"><center> &nbsp&nbsp &nbsp &nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp<input type="+"submit"+" value="+"reply"+" class="+"k1"+";></center></fieldset></fieldset></form>";
+   
+    
+
+    
+}
+function custom4(){
+
+    var a3=document.getElementById("red4")
+    a3.innerHTML="<form><fieldset class="+"b1"+"><fieldset class="+"a2"+"></fieldset><fieldset class="+"g1"+"> <strong class ="+"d1"+"></strong><br><br><br><form><textarea class="+"i1"+" ></textarea></fieldset><fieldset class="+"a1"+"><center> &nbsp&nbsp &nbsp &nbsp &nbsp&nbsp&nbsp&nbsp &nbsp&nbsp<input type="+"submit"+" value="+"reply"+" class="+"k1"+";></center></fieldset></fieldset></form>";
+   
+    
+
+    
+}
+
+
+function custom5(){
+     var z =0;
+
+    var a3=document.getElementById("red4")
+    z++;
+    a3.innerText=z;
+   
+    
+
+    
+}
+function custom6(){
+    
+
+   var a3=document.getElementById("red4")
+   var z1=0;
+   a3.innerText=z1;
+  
+   
+
+   
+}
+
+
+
+function must1(){
+    var x1 =0;
+
+   var m1=document.getElementById("blue1")
+   x1++;
+   m1.innerText=x1;
+  
+   
+
+   
+}
+function must2(){
+   
+
+  var m2=document.getElementById("blue1")
+  var x2=0;
+  m2.innerText=x2;
+ 
+  
+
+  
+}
+ function must3(){
+   
+
+    var m3=document.getElementById("blue3")
+    var x3=0;
+    x3++;
+    m3.innerText=x3;
+   
+    
+  
+    
+  }
+  function must4(){
+   
+
+    var m4=document.getElementById("blue3")
+    var x4=0;
+    m4.innerText=x4;
+   
+    
+  
+    
+  }
+  function must5(){
+   
+
+    var m5=document.getElementById("blue5")
+    var x5=0;
+    x5++;
+    m5.innerText=x5;
+   
+    
+  
+    
+  }
+  function must6(){
+   
+
+    var m6=document.getElementById("blue5")
+    var x6=0;
+    m6.innerText=x6;
+   
+    
+  
+    
+  }
+
+  function custom90(){
+
+    var m7=document.getElementById("delete")
+    m7.innerHTML="<center><mark>deleted successfully</mark><center>"
+   
+    
+
+    
+}
+function custom91()
+{
+  var m8 =document.getElementById("delete")
+  m8.innerHTML=" <fieldset id="+"delete"+"><fieldset class="+"b1"+"><fieldset class="+"a1"+"></fieldset><fieldset class="+"a1"+"> <input type="+"button"+"  value ="+"+"+"  name="+"blue5"+"><br><h1 id="+"blue5"+">0</h1><input type="+"button"+"  value ="+"-"+" onclick="+"must6()"+" name="+"blue5"+"></fieldset><fieldset class="+"h1"+"> <strong class ="+"d1"+">&nbsp &nbsp &nbsp &nbsp juliusomo  &nbsp &nbsp &nbsp &nbsp </strong> <input type="+"button"+" value="+"You"+"  >&nbsp &nbsp 2 day ago<input type="+"button"+" value="+"Delete"+" class ="+"j1"+" onclick="+"custom90()"+"  >&nbsp &nbsp &nbsp &nbsp<input type="+"button"+" value="+"Edit"+"  class ="+"j1"+" onclick="+"custom91()"+" ><mark>Do changes here</mark><br><br><br><form><textarea class="+"i1"+" ></textarea><input type="+"button"+" value="+"Update"+" class="+"j1"+"  ></form></fieldset></fieldset>"
+}
